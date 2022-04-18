cf13f943f8b1a3872ac546f8fcdfe4b001a52645
Gan4x4 <gan4x4@gmail.com>
2022-04-18 21:16:14 +0300

    Fix service name in proto file

--- grpc.sh
+++ grpc.sh
@@ -4,14 +4,11 @@ python3 -m venv venv
 
 mkdir -p ./src/generated
 
-#./venv/bin/pip3 install --upgrade pip
 ./venv/bin/python3 -m ensurepip --upgrade
 
-./venv/bin/pip3 install --no-cache-dir -r req.txt
-./venv/bin/pip3 install grpcio
-./venv/bin/pip3 install grpcio-tools
+./venv/bin/pip3 install --no-cache-dir -r src/req.txt
 
 
 ./venv/bin/python -m grpc_tools.protoc -I. --python_out=./src/generated --grpc_python_out=./src/generated DummyService.proto
-#cd src
-./venv/bin/python index.py
+cd src
+../venv/bin/python index.py
