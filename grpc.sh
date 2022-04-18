c8b93e3fe382c516066bd024dcc34a6f630b8f8c
Gan4x4 <gan4x4@gmail.com>
2022-04-18 21:04:37 +0300

    First fixes

--- grpc.sh
+++ grpc.sh
@@ -1,5 +1,17 @@
 #!/bin/bash
 
+python3 -m venv venv
+
 mkdir -p ./src/generated
 
-./venv/bin/python -m grpc_tools.protoc -I. --python_out=./src/generated --grpc_python_out=./src/generated FMBService.proto
+#./venv/bin/pip3 install --upgrade pip
+./venv/bin/python3 -m ensurepip --upgrade
+
+./venv/bin/pip3 install --no-cache-dir -r req.txt
+./venv/bin/pip3 install grpcio
+./venv/bin/pip3 install grpcio-tools
+
+
+./venv/bin/python -m grpc_tools.protoc -I. --python_out=./src/generated --grpc_python_out=./src/generated DummyService.proto
+#cd src
+./venv/bin/python index.py
